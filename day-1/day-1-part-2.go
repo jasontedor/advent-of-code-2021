dc83a58130a99b339d885765b948bc7db20993c9
Jason Tedor <jason@tedor.me>
2021-12-01 12:00:34 -0500

    Solutions for day 1

---
+++ day-1/day-1-part-2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("expected 1 arg, was %d", len(os.Args) - 1)
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// assume the values all fit in memory
+	count := 0
+	var values []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		value, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		values = append(values, value)
+	}
+
+	for i := 3; i < len(values); i++ {
+		if values[i-2]+values[i-1]+values[i] > values[i-3]+values[i-2]+values[i-1] {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
