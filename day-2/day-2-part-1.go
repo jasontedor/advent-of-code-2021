86eaed94eb45344d88d058bcac0fd93cb4f58e49
Jason Tedor <jason@tedor.me>
2021-12-02 08:06:02 -0500

    Solutions for day 2

---
+++ day-2/day-2-part-1.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("expected 1 arg, was %d", len(os.Args) - 1)
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	h, d := 0, 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) != 2 {
+			log.Fatal("expected 2 fields, was %d, %s", len(fields), fields)
+		}
+		mag, err := strconv.Atoi(fields[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if fields[0] == "forward" {
+			h += mag
+		} else if fields[0] == "up" {
+			d -= mag
+		} else if fields[0] == "down" {
+			d += mag
+		} else {
+			log.Fatal("unexpected command, was %s", fields[0])
+		}
+	}
+
+	fmt.Println(h * d)
+}
